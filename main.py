4c181b6d8d2642b1f817a5051f57a4198292fc56
Lnardon <lucaslnardon@gmail.com>
2020-07-31 02:43:23 -0300

    Added ability to stay running and clear the terminal evey after command

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Import os
+import os
 # Import Yeelight Python Lib
 from yeelight import Bulb
 
@@ -5,20 +7,25 @@ from yeelight import Bulb
 bulb = Bulb("192.168.15.3")
 
 # Receives an input from the user and identifies the respective action to take
-print("Select your action:")
-print("Type 1 and hit enter to turn on")
-print("Type 2 and hit enter to turn off")
-print("Type 3 and hit enter to change the brightness level")
-mode = input('Selected Action: ')
+while True:
+    print("Select your action:")
+    print("Type 0 and hit enter to exit")
+    print("Type 1 and hit enter to turn on")
+    print("Type 2 and hit enter to turn off")
+    print("Type 3 and hit enter to change the brightness level")
+    mode = input('Selected Action: ')
 
-# For now it only turns on and off the bulb
-if mode == '1':
-    bulb.turn_on()
-elif mode == '2':
-    bulb.turn_off()
-elif mode == '3':
-    print("Type the value of the desired brightness")
-    brightness = input('Value Between 1 & 100: ')
-    bulb.set_brightness(int(brightness))
-else:
-    print("Action Ignored")
+    # For now it only turns on and off the bulb
+    if mode == '0':
+        exit()
+    elif mode == '1':
+        bulb.turn_on()
+    elif mode == '2':
+        bulb.turn_off()
+    elif mode == '3':
+        print("Type the value of the desired brightness")
+        brightness = input('Value Between 1 & 100: ')
+        bulb.set_brightness(int(brightness))
+    else:
+        print("Invalid Action")
+    os.system('cls' if os.name == 'nt' else 'clear')
